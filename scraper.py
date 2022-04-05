57d4d1af3b0a79be7cec5903e49f9a2a6654466b
janskwr <janskwarek@protonmail.ch>
2022-04-05 19:27:23 +0200

    snscrape usage template.

--- scraper.py
+++ scraper.py
@@ -7,14 +7,49 @@ tweets_list1 = []
 
 # Using TwitterSearchScraper to scrape data and append tweets to list
 for i, tweet in enumerate(sntwitter.TwitterSearchScraper(
-        'Bucha since:2022-04-01 until:2022-04-05')
-                                  .get_items()):  # declare a username
-    if i > 100:  # number of tweets you want to scrape
+    # UWAGA
+    # TUTAJ MOŻNA WRZUCAĆ ARGUMENTY INNE NIŻ SINCE I UNTIL
+    # DA SIĘ JAKOŚ WYSZUKIWAĆ PO HASHTAGACH A NAWET PO LOKALIZACJI, NAZWIE UZYTKOWNIKA, WSPOLRZEDNYCH GEOGRAFICZNYCH,
+        # MIASTACH KONKRETNYCH
+    # GOOGLUJCIE ALBO DAWAJCIE ZNAKA CO CHCECIE TO POSZUKAM SAM
+        'Ukraine since:2022-03-01 until:2022-03-03')
+                                  .get_items()):
+    if i > 10:  # number of tweets you want to scrape
         break
     tweets_list1.append(
-        [tweet.date, tweet.id, tweet.content, tweet.user.username])  # declare the attributes to be returned
+        [tweet.date,
+         tweet.id,
+         tweet.content,
+         tweet.user.username,
+         tweet.replyCount,
+         tweet.retweetCount,
+         tweet.likeCount,
+         tweet.quoteCount,
+         tweet.lang,
+         # SOURCE JEST CALKIEM DUZE, POTRZEBUJEMY TEGO??
+         tweet.source,
+         tweet.retweetedTweet,
+         tweet.quotedTweet,
+         # W KOLEJNEJ LINIJCE LISTA OBJEKTÓW TYPU USER, JAK ZWROCIC TYLKO USERNAME?
+         # [o.username for o in tweet.mentionedUsers if tweet.mentionedUsers is not None]
+         ])  # declare the attributes to be returned
 
 # Creating a dataframe from the tweets list above
-tweets_df1 = pd.DataFrame(tweets_list1, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+tweets_df1 = pd.DataFrame(tweets_list1, columns=['Datetime',
+                                                 'Tweet Id',
+                                                 'Text',
+                                                 'Username',
+                                                 'Replies Count',
+                                                 'Retweets Count',
+                                                 'Likes Count',
+                                                 'Quotes Count',
+                                                 'Language',
+                                                 'Source',
+                                                 'Retweeted Tweet',
+                                                 'Quoted Tweet',
+                                                 # JAK OGARNIE KTOS TO mentionedUsers ZEBY TYLKO USERNAMY ZWRACALO TO
+                                                 # MOZNA ODKOMENTOWAC
+                                                 # 'Mentioned Users'
+                                                 ])
 
 tweets_df1.to_csv('twitter_data1.csv')
